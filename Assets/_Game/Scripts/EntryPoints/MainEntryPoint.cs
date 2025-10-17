4e520e9714a50e8e8732ef5df73719dae5d96756
Fantomext <Fantomext80@gmail.com>
2025-10-17 08:30:07 +0300

    add dotween and scene loader

---
+++ Assets/_Game/Scripts/EntryPoints/MainEntryPoint.cs
@@ -0,0 +1,29 @@
+﻿using Cysharp.Threading.Tasks;
+using VContainer.Unity;
+
+namespace _Game.Scripts.EntryPoints
+{
+    public class MainEntryPoint : IInitializable
+    {
+        public MainEntryPoint()
+        {
+            
+        }
+
+        public async void Initialize()
+        {
+            await InitAsync();
+            Init();
+        }
+
+        public async UniTask InitAsync()
+        {
+            
+        }
+
+        public void Init()
+        {
+            
+        }
+    }
+}
